801d7c44ea1a329beb747ae8c61c349136c5821f
neshubean <annesha.g@berkeley.edu>
2022-06-12 11:18:17 -0400

    to do

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import "./AddTodo.js";
+import "./todos.js";
+import "./Home.css";
   
 class Home extends Component {
   // A default state of this component with an empty list of todos.
   constructor() {
     super();
     this.state = {
+      todos : []
       // create your empty list here call it todos.
     };
   }
@@ -28,7 +32,10 @@ class Home extends Component {
     return (
       <div className="Home">
         <h1>Todo's </h1>
-        <p> Replace this</p>
+        <p>
+          <Todos todos={this.state} />
+          <AddTodo addTodo={this.addTodo} />
+        </p>
       </div>
     );
   }
